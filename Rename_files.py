79cca9ad067716baf2dccd1e7b641fe82b45d59c
Abhi <abhiramims333@gmail.com>
2022-11-25 15:31:35 -0500

    "Debugged issue with file being open while renaming"

--- Rename_files.py
+++ Rename_files.py
@@ -6,8 +6,8 @@ ext=input("Enter file extension: ")
 new_name=input("Enter new name: ")
 c=1
 for i in range(first,last+1):
-    if os.path.exists("IMG_"+str(i)+"."+ext):
-       with open("IMG_"+str(i)+"."+ext) as file:
-          os.rename(file,new_name+" "+str(c)+"."+ext)
+    file_name="IMG_"+str(i)+"."+ext
+    if os.path.exists(file_name):
+          os.rename(file_name,new_name+" "+str(c)+"."+ext)
           c+=1
 print("Number of files renamed : "+str(c))
